0f2d7338f07212a12e0e3c2a79c2b485225d2447
Trung Nam <trungnam.work@gmail.com>
2024-10-19 16:22:05 +0700

    Create basic layout

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,10 @@ import { deepOrange, teal, cyan, orange } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 // Create a theme instance.
 const theme = createTheme({
+  workSmart: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
